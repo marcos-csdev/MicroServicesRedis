ba6e7193de0685867e6960ef4af3e7bf3764f587
Marcos Freire <marcos.freire.csdev@gmail.com>
2024-01-28 20:15:56 -0800

    Add project files.

---
+++ Services/CatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using CatalogAPI.Models;
+using MongoDB.Driver;
+
+namespace CatalogAPI.Data
+{
+    public class CatalogContext
+    {
+        public IMongoCollection<Product> Products { get;  }
+
+
+        public CatalogContext(IConfiguration configuration) 
+        {
+            var client = new MongoClient(GetConfigValue(configuration, "ConnectionString"));
+
+            var db = client.GetDatabase(GetConfigValue(configuration, "DatabaseName"));
+
+            Products = db.GetCollection<Product>(GetConfigValue(configuration, "CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+
+        }
+
+        private string? GetConfigValue(IConfiguration configuration, string value)
+        {
+            return configuration.GetValue<string>($"DatabaseSettings:{value}");
+        }
+    }
+}
---
+++ Services/CatalogAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CatalogAPI.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string? Summary { get; set; }
+        public string? Description { get; set; }
+        public string? ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
